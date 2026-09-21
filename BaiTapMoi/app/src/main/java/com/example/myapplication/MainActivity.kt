22d451c9a5e0fa1ca6a779d634ebc61182ae8b65
NguyenNhuanTri2911 <nhuantri29112006@gmail.com>
2026-09-21 14:56:07 +0700

    update

--- BaiTapMoi/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ BaiTapMoi/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,6 @@
+
 package com.example.myapplication
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -10,12 +12,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        binding.txtKetQua.apply {
+            text = "Kết quả"
+            textSize = 20f
+        }
 
         binding.btnHienThi.setOnClickListener {
-            val ten = binding.edtTen.text.toString()
-            binding.txtKetQua.text = "Xin chào $ten"
+
+            binding.edtTen.text.toString().trim().let { ten ->
+
+                if (ten.isNotEmpty()) {
+                    binding.txtKetQua.text = "Xin chào $ten"
+                } else {
+                    binding.txtKetQua.text = "Bạn chưa nhập tên!"
+                }
+            }
         }
     }
 }
